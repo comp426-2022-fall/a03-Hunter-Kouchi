02dda8215543e7b1429c420cffdaaf56770cb354
Hunter Kouchi <hhun@email.unc.edu>
2022-11-10 18:19:06 -0500

    done

--- bin/cli.js
+++ bin/cli.js
@@ -1,7 +1,11 @@
 #!/usr/bin/env node
 import minimist from "minimist";
-import { roll } from "/lib/roll.js";
+import { roll } from "../lib/roll.js";
 
 const args = minimist(process.argv.slice(2));
 
-console.log(roll(args.sides, args.dice, args.rolls));
+const sides_t = args.sides || 6
+const dice_t = args.dice || 2
+const rolls_t = args.rolls || 1
+
+console.log(roll(sides_t, dice_t, rolls_t));
